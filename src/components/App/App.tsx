ad0f6fd88918ac25f85412f2103a30efaa11e66d
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-20 14:46:31 +0300

    Добавляет компонент поиска

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import Header from '../Header/Header';
+import Search from '../Search/Search';
 
 function App() {
   return (
     <>
       <Header />
+      <Search />
     </>
   );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,12 @@
+import './Search.scss';
+
+function Search() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="What test are you looking for?" />
+      <span>tests</span>
+    </div>
+  );
+}
+
+export default Search;
