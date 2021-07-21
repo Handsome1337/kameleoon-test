83fbac5c0c789661b7c3a562ef6bef83386716dd
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-21 15:08:47 +0300

    Добавляет тень к строке, находящейся в фокусе, при взаимодействии с клавиатурой

--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {IRow} from '../../types';
 import './TableRow.scss';
@@ -8,12 +8,18 @@ interface ITableRowProps {
 }
 
 const TableRow: React.FC<ITableRowProps> = ({data}) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   const {id, name, type, status, site} = data;
 
   const actionText = status === 'Draft' ? 'finalize' : 'results';
 
   return (
-    <tr className={`table-row table-row--${site.split('.').shift()}`}>
+    <tr
+      className={`table-row table-row--${site.split('.').shift()}${isFocused ? ' focused' : ''}`}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    >
       <td className="name-column">{name}</td>
       <td className="type-column">{type}</td>
       <td className={`status-column status-column--${status.toLowerCase()}`}>{status}</td>
